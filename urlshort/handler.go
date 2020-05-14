1281028b5e3c8b99b91cc38738802c17f2081227
ShawkiS <shawkisukkar@gmail.com>
2020-05-14 23:14:19 +0300

    finish url exercise

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -2,6 +2,7 @@ package urlshort
 
 import (
 	"net/http"
+	"gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -38,7 +39,35 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+
+	pathUrls, err := parseYaml(yamlBytes)
+	if err != nil {
+		return nil, err
+	}
+	pathToUrls := buildMap(pathUrls)
+	return MapHandler(pathToUrls, fallback), nil
+}
+
+
+func buildMap(pathUrls []pathUrl) map[string]string {
+	pathToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathToUrls[pu.Path] = pu.Url
+	}
+	return pathToUrls
+}
+
+func parseYaml(data []byte) ([]pathUrl, error){
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
+}
+
+type pathUrl struct  {
+	Path string `yaml:"path"`
+	Url  string `yaml:"url"`
 }
